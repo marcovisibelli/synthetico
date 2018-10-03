74f6cc84bf84ada1b8aa68c46d31567ddc23a8aa
Marco Visibelli <marco.visibelli@vocalink.com>
2018-10-03 11:14:26 +0100

    Release: 2018-10-03

---
+++ Main.py
@@ -0,0 +1,43 @@
+#!/bin/env python
+#----------------------------------------------------------------------------
+# Name:         Main.py
+# Purpose:      Create systetic data for testing machine learning machines
+#
+# Author:       Marco Visibelli
+#
+# Created:      A long time ago, in a galaxy far, far away...
+# Copyright:    (c) 2018 by Total Control Software
+# Licence:      MIT license
+#----------------------------------------------------------------------------
+
+# FIXME List:
+# * Problems with flickering related to ...
+
+
+# TODO List:
+# * creation
+
+
+
+import sys, os
+
+
+def main():
+    print(" --- test --- ")
+
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    __name__ = 'Main'
+    main()
+
+#----------------------------------------------------------------------------
+
+
+
+
+
+
+
